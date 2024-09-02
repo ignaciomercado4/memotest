3070b929c51cd5258447f391c3319833b650b25f
Ignacio Mercado <igmercado46@gmail.com>
2024-09-02 02:18:18 -0400

    añade funciones de manejo de input del usuario + chequeo de coincidencias  + esconder/mostrar emojis

--- src/main.js
+++ src/main.js
@@ -1,26 +1,51 @@
-const $gameBoard = document.querySelector('.game-board')
-const $emojis = document.querySelectorAll(".emoji")
-const $playButton = document.querySelector('#play-button')
-const emojiList = ["😎", "🦀", "🤺", "🐱‍👤"]
-let clickedEmojis = []
-let matches = 0
+const $gameBoard = document.querySelector('.game-board');
+const $emojis = document.querySelectorAll(".emoji");
+const $playButton = document.querySelector('#play-button');
+const emojiList = ["😎", "🦀", "🤺", "🐱‍👤"];
+let clickedEmojis = [];
+let matches = 0;
 
 $playButton.onclick = function () {
-    const shuffledEmojis = shuffleEmojis()
-    
+    const shuffledEmojis = shuffleEmojis();
+
     $emojis.forEach(($emoji, count) => {
         if (!$emoji.classList.contains("opacity-0")) {
-            $emoji.classList.add("opacity-0")
+            $emoji.classList.add("opacity-0");
         }
 
-        $emoji.textContent = shuffledEmojis[count]
-        count++
-    })
+        $emoji.parentElement.parentElement.onclick = handleInput;
 
+        $emoji.textContent = shuffledEmojis[count];
+    });
 }
 
 function shuffleEmojis() {
     let shuffledEmojis = emojiList.concat(emojiList).sort(() => Math.random() - 0.5);
-    
-    return shuffledEmojis
+    return shuffledEmojis;
+}
+
+function handleInput(e) {
+    if (!clickedEmojis.includes(e.target) && clickedEmojis.length < 2 && e.target.classList.contains("emoji")) {
+        clickedEmojis.push(e.target);
+        e.target.classList.replace("opacity-0", "opacity-100");
+        if (clickedEmojis.length === 2) {
+            checkMatch();
+        }
+    }
+}
+
+function checkMatch() {
+    if (clickedEmojis[0].textContent === clickedEmojis[1].textContent) {
+        clickedEmojis = [];
+        matches++;
+    } else {
+        hideEmojis();
+    }
+}
+
+function hideEmojis() {
+    clickedEmojis.forEach((emoji) => {
+        emoji.classList.replace("opacity-100", "opacity-0");
+    });
+    clickedEmojis = [];
 }
